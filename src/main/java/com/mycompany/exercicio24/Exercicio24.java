5da98f7b4c01c0dde190e89dbabd74a8576c8fee
Eduardo Nienkotter Marcelino <110943998+dudunm@users.noreply.github.com>
2025-04-17 19:30:04 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/exercicio24/Exercicio24.java
@@ -0,0 +1,12 @@
+package com.mycompany.exercicio24;
+public class Exercicio24 {
+    public static void main(String[] args) {
+        int inicio = 14;
+        int fim = 23;
+        int contador = inicio;
+        do{
+            System.out.println(contador);
+            contador+=2;
+	}while(contador < fim);
+    }
+}
